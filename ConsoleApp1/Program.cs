b93b369e1b157ec28c8137e50db4e3f347a8777b
winemago <pieromdq@gmail.com>
2021-05-09 23:34:05 -0300

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name = "piero";
+            Console.WriteLine("Hello World!" + name);
+        }
+    }
+}
